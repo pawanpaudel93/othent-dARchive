500f2613a3cc3d16d2211c3525d224a91c717f74
Pawan Paudel <pawanpaudel93@gmail.com>
2023-06-07 23:57:59 +0545

    feat: Add progress bar at the top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import { Providers } from "./providers";
 import NavBar from "@/components/NavBar";
 import { Footer } from "@/components/Footer";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
+          <NextTopLoader />
           <NavBar />
           <div style={{ marginTop: "70px", minHeight: "calc(100vh - 70px)" }}>
             {children}
